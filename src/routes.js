5cc682168e2ef57f7147b53ae89f2b8edb9fe2bf
dyeisonFK <dyeisonfc@gmail.com>
2024-05-28 15:41:03 -0300

    chore: atualização de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,19 @@
 import { Route,Routes, BrowserRouter } from "react-router-dom";
 import ContentInitial from './Routes/Pages/ContentInitial/index'
-import HeaderInitial from './Components/HeaderInitial'
 import Erro from './Routes/Pages/Erro'
 import Login from './Routes/Pages/Login'
 import Register from './Routes/Pages/Register'
+import Footer from './Components/Footer'
 const RoutesApp = () => {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/header" element={<HeaderInitial/>}/>
         <Route path="/" element={<ContentInitial/>}/>
         <Route path="/erro" element={<Erro/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
