ccf84df59a741aa68c7e726a1ad1ab7256c6fb5a
dyeisonFK <dyeisonfc@gmail.com>
2024-05-28 15:41:18 -0300

    feat: adição de código footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import './style.css'
+import Facebook from '../../Images/Site/facebook.png'
+import Instagram from '../../Images/Site/instagram.png'
+import Twitter from '../../Images/Site/twitter.png'
+import Logo from "../../Images/Site/logo.png"
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer id="rodape">
+      <div id="redes-sociais">
+        <img src={Facebook} alt="" />
+        <img src={Twitter} alt="" />
+        <img src={Instagram} alt="" />
+      </div>
 
-export default Footer
+      <h2 id="Copy">₢Copyright - todos os direitos reservados a RDR</h2>
+
+      <img src={Logo} alt="" />
+    </footer>
+  );
+};
+
+export default Footer;
